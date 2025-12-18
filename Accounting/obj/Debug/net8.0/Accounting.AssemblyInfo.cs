12b6308bbb99cbcea28541febc95948034e8e806
TCDDaniyalJaved <ghr111008@technocomdev.com>
2025-12-18 11:41:18 +0500

    DataBase Update

--- Accounting/obj/Debug/net8.0/Accounting.AssemblyInfo.cs
+++ Accounting/obj/Debug/net8.0/Accounting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Accounting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ef15d427d6829d1b8a51a0c3c22ed54fc8b274")]
 [assembly: System.Reflection.AssemblyProductAttribute("Accounting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Accounting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
