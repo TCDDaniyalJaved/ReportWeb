00d0e51d17640a19a3e91cbbf4ba7f80d491b324
TCDDaniyalJaved <ghr111008@technocomdev.com>
2025-12-29 14:39:38 +0500

    update

--- Accounting/Data/DatabaseBackup.bat
+++ Accounting/Data/DatabaseBackup.bat
@@ -8,6 +8,6 @@ SET server_name=.\Sql2022
 REM -- Backup ka SQL command
 sqlcmd -S %server_name% -U %db_user% -P %db_password% -Q "BACKUP DATABASE [%db_name%] TO DISK='%backup_path%'"
 
-REM -- Backup complete hone par message
-echo Bhai Backup complete ho gaaya haii!
+REM -- Message after backup completion
+echo Backup has been completed successfully.
 pause
--- Accounting/obj/Debug/net8.0/Accounting.AssemblyInfo.cs
+++ Accounting/obj/Debug/net8.0/Accounting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Accounting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3651f06aac7e26b65b53051c0007449e9324429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7c72597fd821575c976a4e76ca49a55960ddae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Accounting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Accounting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
